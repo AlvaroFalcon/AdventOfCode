58f3c24e3ae134583d0f3b3596b66f70e1f91e2a
Alvaro Falcón Morales <alvaro@2coders.com>
2018-12-04 11:54:13 +0000

    day one first half completed

--- day_one.py
+++ day_one.py
@@ -1,15 +1,29 @@
-def doOperation(operator, value):
-    if '+' in line:
-        print(line[0])
-    else:
-        print('Resta')
-
-
-file = open('day_one_input.txt','r')
 frequency = 0
+def add(value):
+    global frequency
+    frequency = frequency + value    
 
-        
-for line in file:
-    doOperation(line,line)
+
+def substract(value):
+    global frequency
+    frequency = frequency - value
+
+
+def doOperation(operator, value):    
+    if '+' in operator:
+        add(int(value))
+    else:
+        substract(int(value))
+
+def calculateFrequency():
+    file = open('day_one_input.txt','r')
+
+    for line in file:
+        operator = line[0]
+        value = line[1:]
+        doOperation(operator, value)        
+    print(frequency)
+
+calculateFrequency()
     
     
